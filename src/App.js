d551e0b1dbc64bfad0cdda3f22ed021d81a20505
julia <julia.k@stampli.com>
2021-01-17 21:33:00 +0200

    add dialog windows

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Navigation from "./components/Navigation";
-import TabsContent from "./components/Tabs/TabsContent"
+import TabsContent from "./components/Tabs/TabsContent";
+import AddNewCategory from "./components/Dialogs/AddNewCategory";
+import AddNewCharge from "./components/Dialogs/AddNewCharge";
+import AddNewIncome from "./components/Dialogs/AddNewIncome";
+
 function App() {
   return (
   <div>
       <Navigation />
       <TabsContent />
+      <AddNewCategory />
+      <AddNewCharge />
+      <AddNewIncome />
   </div>
   );
 }
